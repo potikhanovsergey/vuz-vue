7951c93eb9a39cac85c0239fbd4e5e8f2e359b0b
Sergey <potikhanovsergey@gmail.com>
2021-10-09 09:00:13 +0300

    lab-1

---
+++ pages/Article.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="article">
+        <h1>{{ params.name }}</h1>
+        <span>{{ params.date }}</span>
+        <p>{{ params.desc }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    asyncData({ params }) {
+        return {
+            params
+        }
+    }
+}
+</script>
+<style scoped>
+
+.article {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    max-width: 70%;
+    margin: 0 auto;
+}
+
+</style>
